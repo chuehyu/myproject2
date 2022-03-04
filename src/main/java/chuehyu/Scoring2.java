3364b5ae4ddbd8f1d01fe0356ae27c52c6000269
Chuehyu <a230168988@gmail.com>
2022-03-04 16:23:22 +0800

    Student

---
+++ src/main/java/chuehyu/Scoring2.java
@@ -0,0 +1,10 @@
+package chuehyu;
+
+public class Scoring2 {
+    public static void main(String[] args) {
+        Student jack= new Student();
+        jack.name = "Jack";
+        jack.English = 70;
+        jack.math = 88;
+    }
+}
